712a9ae29cca8fb404fdc239145572aa37664c1a
Carlos Chacón <chaconmooncarlos@gmail.com>
2025-02-12 21:17:43 +0100

    Change Tests Path

---
+++ tests/integration_tests/test_bluesky_API.py
@@ -0,0 +1,41 @@
+import pytest
+import os
+from APIs.bluesky import BlueSky
+from dotenv import load_dotenv
+from atproto import Client
+
+
+def test_bluesky_connection_credential_error():
+    load_dotenv()
+    client = BlueSky(os.getenv("BLUESKY_SECUNDARY_NAME"),os.getenv("BLUESKY_SECUNDARY_API_KEY"))
+    assert client.connect() is not None
+
+def test_bluesky_connection_atprotocol_error():
+    client = BlueSky(os.getenv("BLUESKY_SECUNDARY_NAME"),os.getenv("BLUESKY_SECUNDARY_API_KEY"))
+    assert client.connect() is not False
+
+
+def test_bluesky_publish_photo():
+    client = BlueSky(os.getenv("BLUESKY_SECUNDARY_NAME"),os.getenv("BLUESKY_SECUNDARY_API_KEY"))
+    client.connect()
+    assert (
+        client.publish_image(
+            msg="Esta imagen esta siendo usada con fines de testeo.",
+            file="./img/placeholder_testing.jpg",
+            alt_text="Esta imagen esta usado con fines de resteo.",
+        )
+        is True
+    )
+
+
+def test_bluesky_publish_video():
+    client = BlueSky(os.getenv("BLUESKY_SECUNDARY_NAME"),os.getenv("BLUESKY_SECUNDARY_API_KEY"))
+    client.connect()
+    assert (
+        client.publish_video(
+            "Este video esta siendo usado con fines de testeo.",
+            file="./video/video_testing.mp4",
+            alt_text="Este video esta siendo usado con fines de testeo.",
+        )
+        is True
+    )
---
+++ tests/integration_tests/test_twitter_API.py
@@ -0,0 +1,34 @@
+import pytest
+import os
+from dotenv import load_dotenv
+from APIs.twitter import Twitter
+
+
+def test_twiiter_connection():
+    load_dotenv()
+    client = Twitter(
+        os.getenv("TWITTER_PRIMARY_API_KEY"),
+        os.getenv("TWITTER_PRIMARY_API_SECRET_KEY"),
+        os.getenv("TWITTER_PRIMARY_ACCESS_TOKEN"),
+        os.getenv("TWITTER_PRIMARY_ACCESS_TOKEN_SECRET"),
+        os.getenv("TWITTER_BEARER_TOKEN"),
+    )
+    assert client.connect() is True
+
+
+def test_twiiter_publish_image():
+    client = Twitter(
+        os.getenv("TWITTER_PRIMARY_API_KEY"),
+        os.getenv("TWITTER_PRIMARY_API_SECRET_KEY"),
+        os.getenv("TWITTER_PRIMARY_ACCESS_TOKEN"),
+        os.getenv("TWITTER_PRIMARY_ACCESS_TOKEN_SECRET"),
+        os.getenv("TWITTER_BEARER_TOKEN"),
+    )
+    client.connect()
+    assert (
+        client.publish_image(
+            "Asi me imagino la gente cuando le digo que me gusta Just Dance",
+            "./img/test_warioware.jpg",
+        )
+        is True
+    )
