ff1d27c97ac703e7c35a7558fb9469b2935d5a01
Carlos Chacón <chaconmooncarlos@gmail.com>
2025-02-16 12:44:37 +0100

    Change UnitTest module name

--- tests/unit_tests/test_api_code.py
+++ tests/unit_tests/test_api_code.py
@@ -3,12 +3,11 @@ from APIs.bluesky import BlueSky
 from unittest.mock import MagicMock, patch
 from atproto.exceptions import AtProtocolError
 
+
 # Función a parcher, se parchea de arriba a abjo en orden inverso a los @patch
 @patch("APIs.bluesky.Client.login")
 def test_connection_is_none_if_ATProtocolError(mock_client: MagicMock):
-
-    #Provoca que la funcion mockeada salte una excepción
+    # Provoca que la funcion mockeada salte una excepción
     mock_client.side_effect = AtProtocolError
-    connector_bluesky = BlueSky()
-    assert connector_bluesky.connect() is None    
-
+    connector_bluesky = BlueSky("BLUESKY_SECUNDARY_NAME", "BLUESKY_SECUNDARY_API_KEY")
+    assert connector_bluesky.connect() is None
