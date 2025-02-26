283243323619395755f037b23503f6e0af6e3b8b
Carlos Chacón <chaconmooncarlos@gmail.com>
2025-02-26 21:06:37 +0100

    Pubish videos on Twitter

--- src/APIs/twitter.py
+++ src/APIs/twitter.py
@@ -1,5 +1,6 @@
 import tweepy
 from tweepy import Client
+from tweepy.models import Media
 from APIs.api import Api
 import os
 
@@ -15,11 +16,11 @@ class Twitter(Api):
         token_secret: str,
         barrer_token: str,
     ):
-        self.primary_key = os.getenv(primary_key)
-        self.secret_key = os.getenv(secret_key)
-        self.token = os.getenv(token)
-        self.secret_token = os.getenv(token_secret)
-        self.barren_token = os.getenv(barrer_token)
+        self.primary_key = primary_key
+        self.secret_key = secret_key
+        self.token = token
+        self.secret_token = token_secret
+        self.barren_token = barrer_token
 
     def connect(self) -> bool:
         try:
@@ -43,9 +44,9 @@ class Twitter(Api):
         except BaseException as e:
             return print(e)
 
-    def publish_image(self, msg: str, file: str):
+    def publish_image(self, msg: str, file: str, alt_text: str):
         try:
-            photo = self.api.media_upload(filename=file)
+            photo = self.update_media(file)
             self.client.create_tweet(text=msg, media_ids=[photo.media_id])
             return True
         except BaseException as e:
@@ -54,8 +55,17 @@ class Twitter(Api):
     def publish_text():
         pass
 
-    def publish_video():
-        pass
+    def publish_video(self, msg: str, file: str, alt_text: str):
+        try:
+            video = self.update_media(file)
+            self.client.create_tweet(text=msg, media_ids=[video.media_id])
+            return True
+        except BaseException as e:
+            return print(e)
 
     def view_preview():
         pass
+
+    def update_media(self, file: str) -> Media:
+        media = self.api.media_upload(filename=file)
+        return media
