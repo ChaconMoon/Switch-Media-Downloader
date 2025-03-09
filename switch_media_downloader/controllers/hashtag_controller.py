8e0e79108f65e864e40024b13c19e99f1d339bca
Carlos Chacón <chaconmooncarlos@gmail.com>
2025-03-09 21:41:08 +0100

    Add Game Data

--- switch_media_downloader/controllers/hashtag_controller.py
+++ switch_media_downloader/controllers/hashtag_controller.py
@@ -3,7 +3,7 @@ import json
 
 def set_game_hashtag(game_id: str) -> str:
     try:
-        with open("./data/game_hashtag.json") as game_hashtags:
+        with open("./data/game_hashtag_es.json") as game_hashtags:
             hashtag_list = json.loads(game_hashtags.read())
             return f"{hashtag_list[game_id]}"
     except KeyError:
