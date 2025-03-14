d767d2d7841459bcfd170456fba7cd7a74ee0e36
Carlos Chacón <chaconmooncarlos@gmail.com>
2025-03-14 20:33:47 +0100

    Write Unit Test

---
+++ tests/unit_tests/test_hashtag_controllers.py
@@ -0,0 +1,18 @@
+from switch_media_downloader.controllers.hashtag_controller import set_game_hashtag
+
+# METHOD SET_GAME_HASHTAG_TEST
+
+
+# VALIDE ID TEST
+def test_hashtag_get_valid_name():
+    assert set_game_hashtag("B6CE40797459B0890BF7CEF68A4CE587") == "#PokemonPurpura"
+
+
+# INVALIDE ID TEST
+def test_hashtag_get_invalide_name():
+    assert set_game_hashtag("AAAAAAAAAAAAAA") is None
+
+
+# NULL TEST
+def test_hashtag_get_name_none():
+    assert set_game_hashtag(None) is None
---
+++ tests/unit_tests/test_name_file_controller.py
@@ -0,0 +1,52 @@
+from switch_media_downloader.controllers.name_file_controller import (
+    get_file_name,
+    get_game_id,
+)
+
+# GET_FILE_NAME
+
+# VALID NAME TEST
+
+
+def test_get_file_valid_name_image():
+    assert get_file_name("http://192.0.0.1/image.png") == "image.png"
+
+
+def test_get_file_valid_name_video():
+    assert get_file_name("http://192.0.0.1/video.mp4") == "video.mp4"
+
+    # INVALID NAME TEST
+
+
+def test_get_file_invalid_name_image():
+    assert get_file_name("image.png") is None
+
+
+def test_get_file_invalid_name_video():
+    assert get_file_name("video.mp4") is None
+
+
+def test_get_file_invalid_no_name():
+    assert get_file_name("http://192.0.0.1/") is None
+
+    # NULL TEST
+
+
+def test_get_file_null_name():
+    assert get_file_name(None) is None
+
+
+# GET_GAME_ID
+
+
+# VALIDE ID TEST
+def test_game_id_valid_name():
+    assert (
+        get_game_id("2025030319095200-B6CE40797459B0890BF7CEF68A4CE587.jpg")
+        == "B6CE40797459B0890BF7CEF68A4CE587"
+    )
+
+
+# INVALIDE ID TEST
+def test_game_id_none_name():
+    assert get_game_id("2025030319095200") is None
