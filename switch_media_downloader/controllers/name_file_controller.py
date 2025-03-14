916f26208f9058fa2cd1b87c6ce4a68530bd56c8
Carlos Chacón <chaconmooncarlos@gmail.com>
2025-03-14 20:33:47 +0100

    Write Unit Test

--- switch_media_downloader/controllers/name_file_controller.py
+++ switch_media_downloader/controllers/name_file_controller.py
@@ -1,6 +1,18 @@
+import re
+
+
 def get_game_id(file_name: str) -> str:
-    return file_name.split("-")[-1].split(".")[0]
+    if re.search("\\d{16}-\\w{32}[.]{1}\\w{3}", file_name):
+        return file_name.split("-")[-1].split(".")[0]
 
 
 def get_file_name(url_file: str) -> str:
-    return url_file.split("/")[-1]
+    if url_file is None:
+        return None
+    elif re.search(
+        "http://((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])/([a-zA-Z0-9\\-_]+)\\.([a-zA-Z0-9]+)",
+        url_file,
+    ):
+        return url_file.split("/")[-1]
+    else:
+        return None
---
+++ tests/unit_tests/test_hashtag_controllers.py
@@ -0,0 +1,18 @@
+from switch_media_downloader.controllers.hashtag_controller import set_game_hashtag
+
+# METHOD SET_GAME_HASHTAG_TEST
+
+
+# VALID ID TEST
+def test_hashtag_get_valid_name():
+    assert set_game_hashtag("B6CE40797459B0890BF7CEF68A4CE587") == "#PokemonPurpura"
+
+
+# INVALID ID TEST
+def test_hashtag_get_invalid_name():
+    assert set_game_hashtag("AAAAAAAAAAAAAA") is None
+
+
+# NULL TEST
+def test_hashtag_get_name_none():
+    assert set_game_hashtag(None) is None
