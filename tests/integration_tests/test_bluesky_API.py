4a415e0d2e0779c47d2400dc7de0be78960a5cb5
Carlos Chacón <chaconmooncarlos@gmail.com>
2025-03-15 00:22:14 +0100

    Update BlueSky Tests

--- tests/integration_tests/test_bluesky_API.py
+++ tests/integration_tests/test_bluesky_API.py
@@ -5,28 +5,23 @@ from dotenv import load_dotenv
 
 def test_bluesky_connection_credential_error():
     load_dotenv()
-    client = BlueSky(
-        os.getenv("BLUESKY_SECUNDARY_NAME"), os.getenv("BLUESKY_SECUNDARY_API_KEY")
-    )
+    client = BlueSky("BLUESKY_SECUNDARY_NAME", "BLUESKY_SECUNDARY_API_KEY")
     assert client.connect() is not None
 
 
 def test_bluesky_connection_atprotocol_error():
-    client = BlueSky(
-        os.getenv("BLUESKY_SECUNDARY_NAME"), os.getenv("BLUESKY_SECUNDARY_API_KEY")
-    )
+    load_dotenv()
+    client = BlueSky("BLUESKY_SECUNDARY_NAME", "BLUESKY_SECUNDARY_API_KEY")
     assert client.connect() is not False
 
 
 def test_bluesky_publish_photo():
-    client = BlueSky(
-        os.getenv("BLUESKY_SECUNDARY_NAME"), os.getenv("BLUESKY_SECUNDARY_API_KEY")
-    )
+    client = BlueSky("BLUESKY_SECUNDARY_NAME", "BLUESKY_SECUNDARY_API_KEY")
     client.connect()
     assert (
         client.publish_image(
             msg="Esta imagen esta siendo usada con fines de testeo.",
-            file="./img/placeholder_testing.jpg",
+            file="./tests/test_media/placeholder_testing.jpg",
             alt_text="Esta imagen esta usado con fines de resteo.",
         )
         is True
@@ -41,7 +36,7 @@ def test_bluesky_publish_video():
     assert (
         client.publish_video(
             "Este video esta siendo usado con fines de testeo.",
-            file="./video/video_testing.mp4",
+            file="./tests/test_media/video_testing.mp4",
             alt_text="Este video esta siendo usado con fines de testeo.",
         )
         is True
