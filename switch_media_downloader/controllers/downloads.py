01aae3db98a982662783f5b4e168d6016c062caa
Carlos Chacón <chaconmooncarlos@gmail.com>
2025-03-16 14:07:15 +0100

    Create Download Tests

--- switch_media_downloader/controllers/downloads.py
+++ switch_media_downloader/controllers/downloads.py
@@ -6,44 +6,50 @@ from controllers.name_file_controller import get_game_id, get_file_name
 
 
 def download_file(url_file: str) -> str:
-    file_request = request_file(url_file)
-    if file_request.status_code == 200:
-        file_name = get_file_name(url_file)
-    game_id = get_game_id(file_name)
-    extension = f"{file_name.split('.')[-1]}"
-    with open("./data/game_titles.json", encoding="utf-8") as games_buffer:
-        games_list = games_buffer.read()
-        try:
-            base_path = "./img/"
-            if extension == "mp4":
-                base_path = "./video/"
-            list_of_games = json.loads(games_list)
-            game_name = f"{list_of_games[game_id]}"
-            file_name = f"{game_name} {file_name.split('-')[0]}"
-            game_name = re.sub(r"[/\\:*\"<>|]", "", game_name)
-            file_name = re.sub(r"[/\\:*\"<>|]", "", file_name)
+    if url_file is not None:
+        file_request = request_file(url_file)
+        if file_request.status_code == 200:
+            file_name = get_file_name(url_file)
+            game_id = get_game_id(file_name)
+            extension = f"{file_name.split('.')[-1]}"
+            with open("./data/game_titles.json", encoding="utf-8") as games_buffer:
+                games_list = games_buffer.read()
+                try:
+                    base_path = "./img/"
+                    if extension == "mp4":
+                        base_path = "./video/"
+                    list_of_games = json.loads(games_list)
+                    game_name = f"{list_of_games[game_id]}"
+                    file_name = f"{game_name} {file_name.split('-')[0]}"
+                    game_name = re.sub(r"[/\\:*\"<>|]", "", game_name)
+                    file_name = re.sub(r"[/\\:*\"<>|]", "", file_name)
 
-            file_path = f"{base_path}{game_name}/{file_name}.{extension}"
-            try:
-                os.makedirs(base_path)
-            except FileExistsError:
-                pass
-            try:
-                os.makedirs(f"{base_path}{game_name}")
-            except FileExistsError:
-                pass
-        except KeyError:
-            print("Ese ID no esta en la lista, se usara el nombre por defecto.")
-            try:
-                os.makedirs(f"{base_path}{game_id}/".encode())
-            except FileExistsError:
-                pass
-            file_path = f"{base_path}{game_id}/{file_name}".encode()
-    with open(file_path, "wb") as file:
-        file.write(file_request.content)
-    return file_path
+                    file_path = f"{base_path}{game_name}/{file_name}.{extension}"
+                    try:
+                        os.makedirs(base_path)
+                    except FileExistsError:
+                        pass
+                    try:
+                        os.makedirs(f"{base_path}{game_name}")
+                    except FileExistsError:
+                        pass
+                except KeyError:
+                    print("Ese ID no esta en la lista, se usara el nombre por defecto.")
+                    try:
+                        os.makedirs(f"{base_path}{game_id}/".encode())
+                    except FileExistsError:
+                        pass
+                    file_path = f"{base_path}{game_id}/{file_name}".encode()
+            with open(file_path, "wb") as file:
+                file.write(file_request.content)
+            return file_path
+    else:
+        return None
 
 
 def request_file(url_file: str):
-    print(f"Downloading: {url_file}")
-    return requests.get(url_file)
+    if url_file is not None:
+        print(f"Downloading: {url_file}")
+        return requests.get(url_file)
+    else:
+        return None
--- tests/integration_tests/test_downloads.py
+++ tests/integration_tests/test_downloads.py
@@ -2,16 +2,53 @@ from switch_media_downloader.controllers.downloads import download_file, request
 
 
 def test_download_file_valid_image():
-    assert download_file()
+    assert (
+        download_file(
+            "https://github.com/ChaconMoon/Switch-Media-Downloader/blob/master/tests/test_media/2024100319575600-DB679239AE5C0DC0D5E47C22D6492D98.jpg"
+        )
+        == "./img/WarioWare Move It!/WarioWare Move It! 2024100319575600.jpg"
+    )
 
 
-def test_download_file_valid_video():
-    pass
+def test_download_file_invalid_image():
+    assert (
+        download_file(
+            "https://github.com/ChaconMoon/Switch-Media-Downloader/blob/master/tests/test_media/nothing.jpg"
+        )
+        is None
+    )
 
 
-def test_download_file_invalid():
-    pass
+def test_download_file_invalid_video():
+    assert (
+        download_file(
+            "https://github.com/ChaconMoon/Switch-Media-Downloader/blob/master/tests/test_media/nothing.mp4"
+        )
+        is None
+    )
 
 
 def test_download_file_none():
-    pass
+    download_file(None) is None
+
+
+def test_request_file_valid_file():
+    assert (
+        request_file(
+            "https://github.com/ChaconMoon/Switch-Media-Downloader/blob/master/tests/test_media/2024100319575600-DB679239AE5C0DC0D5E47C22D6492D98.jpg"
+        ).status_code
+        == 200
+    )
+
+
+def test_request_file_invalid_file():
+    assert (
+        request_file(
+            "https://github.com/ChaconMoon/Switch-Media-Downloader/blob/master/tests/test_media/2024100319575600-nothing.jpg"
+        ).status_code
+        == 404
+    )
+
+
+def test_request_file_none():
+    assert request_file(None) is None
