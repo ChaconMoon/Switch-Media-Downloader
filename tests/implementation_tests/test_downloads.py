b6a6fdbf34fc1f543021390b097c84f070a35f23
Carlos Chacón <chaconmooncarlos@gmail.com>
2025-03-21 18:15:25 +0100

    Change Test Media Path

--- tests/implementation_tests/test_downloads.py
+++ tests/implementation_tests/test_downloads.py
@@ -5,7 +5,7 @@ from switch.downloaders.image import get_switch_images
 def test_download_file_valid_image():
     image_list = list()
     image_list.append(
-        "https://raw.githubusercontent.com/ChaconMoon/Switch-Media-Downloader/master/tests/test_media/2024100319575600-DB679239AE5C0DC0D5E47C22D6492D98.jpg"
+        "https://raw.githubusercontent.com/ChaconMoon/TESTING/main/Switch_Media_Downloader/2024100319575600-DB679239AE5C0DC0D5E47C22D6492D98.jpg"
     )
     photo_path = get_switch_images(image_list)
     assert (
@@ -17,7 +17,7 @@ def test_download_file_valid_image():
 def test_download_file_invalid_image():
     assert (
         download_file(
-            "https://raw.githubusercontent.com/ChaconMoon/Switch-Media-Downloader/master/tests/test_media/20241003195750-DB679239AE5C0DC0D5E47C22D6492D98.jpg"
+            "https://raw.githubusercontent.com/ChaconMoon/TESTING/main/Switch_Media_Downloader/2024100319575600-DB679239DC0D5E47C22D6492D98.jpg"
         )
         is None
     )
@@ -34,15 +34,6 @@ def test_download_file_invalid_video():
 
 def test_download_file_none():
     download_file(None) is None
-
-
-def test_request_file_valid_file():
-    assert (
-        request_file(
-            "https://github.com/ChaconMoon/Switch-Media-Downloader/blob/master/tests/test_media/2024100319575600-DB679239AE5C0DC0D5E47C22D6492D98.jpg"
-        ).status_code
-        == 200
-    )
 
 
 def test_request_file_invalid_file():
