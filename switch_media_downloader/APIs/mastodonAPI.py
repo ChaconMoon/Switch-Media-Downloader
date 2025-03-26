4b5772ae9c95182deff51d1aebfec4242583a9f2
Carlos Chacón <chaconmooncarlos@gmail.com>
2025-03-26 13:54:41 +0100

    Add Video compatibility in Mastodon

--- switch_media_downloader/APIs/mastodonAPI.py
+++ switch_media_downloader/APIs/mastodonAPI.py
@@ -1,3 +1,4 @@
+import time
 from mastodon import Mastodon
 
 from dotenv import load_dotenv
@@ -35,7 +36,16 @@ class MastodonAPI(Api):
         self.client.toot(status=msg)
 
     def publish_video(self, msg, file, alt_text):
-        return super().publish_video(msg, file, alt_text)
+        video = self.client.media_post(
+            media_file=file, description=alt_text, mime_type="video/mp4"
+        )
+        info_media = self.client.media(video)
+
+        while info_media["url"] is None:
+            time.sleep(5)
+            info_media = self.client.media(video)
+
+        self.client.status_post(status=msg, media_ids=video.id)
 
     def view_preview(self, msg, file, alt_text):
         return super().view_preview(msg, file, alt_text)
--- tests/implementation_tests/test_mastdodon_API.py
+++ tests/implementation_tests/test_mastdodon_API.py
@@ -29,3 +29,20 @@ def test_mastodon_publish_image():
         file="./tests/test_media/2024100319575600-DB679239AE5C0DC0D5E47C22D6492D98.jpg",
         alt_text="Que mono es 9-Volt",
     )
+
+
+def test_mastodon_publish_video():
+    api = MastodonAPI(
+        client_id="APPLICATION_ID_MASTODON",
+        secret="SECRET_MASTODON",
+        access_token="ACCESS_TOKEN_MASTODON",
+        mastodon_instance="MASTODON_INSTANCE",
+    )
+
+    api.connect()
+
+    api.publish_video(
+        msg="En teoria esto se tendria que publicar en cuanto la URL del video exista es decir ya soy capaz de saber cuando el video esta subido, en teoria",
+        file="./tests/test_media/Mario & Luigi Brothership 2024111516292701.mp4",
+        alt_text="Que buen golpe",
+    )
