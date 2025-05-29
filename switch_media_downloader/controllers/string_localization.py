57bea19df3fd6a934283cba51f712bcc4225ca7b
Carlos Chacón <chaconmooncarlos@gmail.com>
2025-05-29 11:27:40 +0200

    Move Transalation Data to CSV

--- switch_media_downloader/controllers/string_localization.py
+++ switch_media_downloader/controllers/string_localization.py
@@ -1,4 +1,5 @@
-import json
+import os
+import pandas as pd
 from importlib.resources import files
 from switch_media_downloader.controllers.lenguages_settings_controller import (
         LenguagesController,
@@ -10,24 +11,23 @@ class StringLocalization:
         _instance = None
 
         def get_localizated_string(self, string_id: str) -> str:
-                return self.localizated_strings[string_id]
+                return self.localizated_strings[string_id, self.actual_localization]
 
         def set_string_file(self):
+                base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+                csv_path = os.path.join(base_dir, "data", "program_strings.csv")
+                strings_data_frame = pd.read_csv(csv_path)
+                strings_data_frame.set_index("COLUM_ID", inplace=True)
+                self.localizated_strings = strings_data_frame.loc
                 lenguages_controller = LenguagesController()
                 if lenguages_controller.lang == Lenguage.ESP:
-                        strings_file_path = files("switch_media_downloader").joinpath(
-                                "data/strings_es.json"
-                        )
+                        self.actual_localization = "ESP"
                 elif lenguages_controller.lang == Lenguage.ENG:
-                        strings_file_path = files("switch_media_downloader").joinpath(
-                                "data/strings_en.json"
-                        )
-                with open(strings_file_path, encoding="utf-8") as strings_paths:
-                        self.localizated_strings = json.loads(strings_paths.read())
+                        self.actual_localization = "ENG"
 
         def __new__(cls):
                 if cls._instance is None:
-                        cls._instance = super(StringLocalization, cls).__new__(cls)
+                        cls._instance = super().__new__(cls)
                 return cls._instance
 
         def __init__(self):
